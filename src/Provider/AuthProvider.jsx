97f92f14506f05064a76d8e91c7afa81b0bfa342
sajib689 <jibonraja11@gmail.com>
2024-09-26 16:18:09 +0600

    add the googleProvider and the gitHubProvider

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from "react";
 import auth from './../Firebase/Firebase.config';
-import { onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
+import { GithubAuthProvider, onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
+import { GoogleAuthProvider } from "firebase/auth";
 
 export const AuthContext = null;
 const AuthProvider = ({children}) => {
     const [user, setUser] = useState(null)
     const [loading, setLoading] = useState(true)
-
+    const googleProvider = new GoogleAuthProvider()
+    const gitHubProvider = new GithubAuthProvider()
     const createAccount = (email,password) => {
         setLoading(true)
         return createUserWithEmailAndPassword(auth,email,password)
