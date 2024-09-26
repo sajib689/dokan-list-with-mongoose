77b85d7cf60948119931dfdd6e902529240e084b
sajib689 <jibonraja11@gmail.com>
2024-09-26 16:23:21 +0600

    add the authProvider in main.jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import AuthProvider from './Provider/AuthProvider.jsx'
+import Main from './Main/Main.jsx'
 
 createRoot(document.getElementById('root')).render(
   <>
-    <App />
+    <AuthProvider>
+      <Main/>
+    </AuthProvider>
   </>,
 )
