d8870744557560f06e0c6dd6e74c4a92d274d556
sajib689 <jibonraja11@gmail.com>
2024-09-26 17:10:52 +0600

    add the router provider

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import AuthProvider from './Provider/AuthProvider.jsx'
 import Main from './Main/Main.jsx'
+import { RouterProvider } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <>
     <AuthProvider>
+      <RouterProvider>
       <Main/>
+      </RouterProvider>
     </AuthProvider>
   </>,
 )
